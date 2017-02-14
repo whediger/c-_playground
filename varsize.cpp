aef3f4b7d4eed9fd7d8c831f114873aa3eddac62
Wes Hediger <wes.hediger@gmail.com>
2017-02-14 13:43:52 -0700

    add varsize

---
+++ varsize.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "The size of an int is: " << sizeof(int) << " bytes.\n";
+  cout << "The size of a short is: " << sizeof(short) << " bytes.\n";
+  cout << "The size of a long is: " << sizeof(long) << " bytes. \n";
+  cout << "The size of a char is: " << sizeof(char) << " bytes.\n";
+  cout << "The size of a float is: " << sizeof(float) << " bytes.\n";
+  cout << "Th soze of a double is: " << sizeof(double) << " bytes.\n";
+
+  return 0;
+}
